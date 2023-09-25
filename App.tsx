0b49080e6f8398e8ad27447d465817c4bd014d57
khanakia <khanakia@gmail.com>
2023-09-25 18:51:34 +0530

    adding pushpender-singh-ap/react-native-scanner

--- App.tsx
+++ App.tsx
@@ -1,118 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  Alert,
+  Platform,
+  useWindowDimensions,
   Text,
-  useColorScheme,
-  View,
+  SafeAreaView
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { request, PERMISSIONS, openSettings, RESULTS } from 'react-native-permissions';
+import { ReactNativeScannerView } from "@pushpendersingh/react-native-scanner";
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+export default function App() {
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  const { height, width } = useWindowDimensions();
+  const [isCameraPermissionGranted, setIsCameraPermissionGranted] = useState(false);
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    checkCameraPermission();
+  }, []);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const checkCameraPermission = async () => {
+    request(Platform.OS === 'ios' ? PERMISSIONS.IOS.CAMERA : PERMISSIONS.ANDROID.CAMERA)
+      .then(async (result: any) => {
+        switch (result) {
+          case RESULTS.UNAVAILABLE:
+            // console.log('This feature is not available (on this device / in this context)');
+            break;
+          case RESULTS.DENIED:
+            Alert.alert("Permission Denied", "You need to grant camera permission first");
+            openSettings();
+            break;
+          case RESULTS.GRANTED:
+            setIsCameraPermissionGranted(true);
+            break;
+          case RESULTS.BLOCKED:
+            Alert.alert("Permission Blocked", "You need to grant camera permission first");
+            openSettings();
+            break;
+        }
+      })
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  if (isCameraPermissionGranted) {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <ReactNativeScannerView
+          style={{ height, width }}
+          onQrScanned={(value: any) => {
+            console.log(value.nativeEvent);
+          }}
+        />
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <Text style={{ fontSize: 30, color: 'red' }}>
+        You need to grant camera permission first
+      </Text>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
